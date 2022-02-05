9db7954cc18baf61b7b8d62807b443f313f45336
trzysiek <krzysztof.kiewicz@lizardfs.com>
2022-02-05 21:38:20 +0100

    Add simple generator with 1 test poems' lines

---
+++ README.md
@@ -0,0 +1,2 @@
+How to use: ./poemsGenerator.py < inputFile.in
+e.g. ./poemsGenerator.py < testPoems.in
---
+++ poemsGenerator.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import random
+import requests
+import sys
+
+def randomWikipediaTitle():
+    S = requests.Session()
+    URL = "https://en.wikipedia.org/w/api.php"
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "list": "random",
+        "rnnamespace": "0"
+    }
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+    return DATA["query"]["random"][0]["title"]
+
+# reads input in format
+# (empty line)
+# N (integer)
+# first line
+# second line
+# ...
+# N-th line 
+def readOneTable():
+    sys.stdin.readline()
+    choicesNr = int(sys.stdin.readline())
+    lines = []
+    for i in range(choicesNr):
+        lines.append(sys.stdin.readline())
+    return lines
+
+# linesList - [[line11, line12, line13, ...], [line21, line22, ...], ...]
+def randomPoem(linesList):
+    poem = "Poem about " + randomWikipediaTitle() + "\n"
+    for l in linesList:
+        poem += random.choice(l)
+    poem = poem[:-1]
+    return poem
+
+def __main__():
+    linesNr = int(sys.stdin.readline())
+    lines = []
+    for i in range(linesNr):
+        lines += [readOneTable()]
+    print(randomPoem(lines))
+
+__main__()
